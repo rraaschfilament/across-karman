e68509c14f5cde076821bc784d009a329b1feaea
Ray Raasch <rraasch@filamentgames.com>
2023-07-26 15:58:38 -0500

    7/26 Finalized copy for splash screen

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -12,8 +12,8 @@ const SplashScreen: React.FC<SplashScreenProps> = ({setSplashScreen}) => {
 
             <div className="splash_screen_text">
                 <div className="splash_screen_title">LAUNCHING INTO ORBIT</div>
-                <div className="splash_screen_subtitle">CATCHY SUBTITLE</div>
-                <div className="splash_screen_body">66 years after the first satellite was successfully launched into orbit, Sputnik I by the Soviet Union, the impact of satellites is more integrated into our life than ever. This interactive will take you through 5 layers of orbit we use for satellites and other space bodies, LEO, MEO, GEO, GSO, and HEO. What do we put into space, and why do we put it where it is?</div>
+                <div className="splash_screen_subtitle">A SPOTLIGHT ON SATELLITES</div>
+                <div className="splash_screen_body">66 years after the first satellite, Sputnik I, was successfully launched into orbit by the Soviet Union, the impact of satellites is more integrated into our lives than ever before. Here we will take you through five layers of orbit we use for satellites and other space bodies to answer the core questions: what do we put into space, and why do we put it where it is?</div>
                 <button className="splash_screen_button" onClick={handleUnsetSplash}>READY TO LAUNCH</button>
             </div>
 
