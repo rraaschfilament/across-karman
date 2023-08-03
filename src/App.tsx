e2482d57cef2e80f12ceb0729b9c876bf1f0ea47
Kristen San Martin <ksanmartin@filamentgames.com>
2023-08-03 11:39:20 -0700

    8-3 - App.tsx - show nav container on mobile splash screen, don't use adjustElementScale for mobile devices, don't reset activeId on window resize for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect } from "react";
 import { Provider, useSelector, useDispatch } from "react-redux";
-import { setActiveId, setHoveringId, setEarthOrbitsScale } from './features/appSlice';
+import {
+  setActiveId,
+  setHoveringId,
+  setEarthOrbitsScale,
+} from "./features/appSlice";
 
 import store, { RootState } from "./app/store";
 import "./App.css";
@@ -17,31 +21,37 @@ import OrbitCloseupText from "./components/OrbitCloseupText";
 import SingleOrbitAnimationDetails from "./components/SingleOrbitAnimationDetails";
 import StaticImgCloseup from "./components/StaticImgCloseup";
 
-
 export const App: React.FC = () => {
   const orbitIds = ["leo", "meo", "heo", "gso", "geo", "gto"];
 
   const dispatch = useDispatch();
 
   const activeId = useSelector((state: RootState) => state.app.activeId);
-  const isSplashScreen = useSelector((state: RootState) => state.app.isSplashScreen);
+  const isSplashScreen = useSelector(
+    (state: RootState) => state.app.isSplashScreen
+  );
   const flyToId = useSelector((state: RootState) => state.app.flyToId);
-  const currentStaticImg = useSelector((state: RootState) => state.app.currentStaticImg);
-  const earthOrbitsScale = useSelector((state: RootState) => state.app.earthOrbitsScale);
-  const isMobileDevice = useIsMobileDevice()
-  const showNavBar = isMobileDevice || (!isSplashScreen && !isMobileDevice);
+  const currentStaticImg = useSelector(
+    (state: RootState) => state.app.currentStaticImg
+  );
+  const earthOrbitsScale = useSelector(
+    (state: RootState) => state.app.earthOrbitsScale
+  );
+  const isMobileDevice = useIsMobileDevice();
+  const showNavBar = isMobileDevice || !isSplashScreen;
 
-  
   const handleResize = () => {
-    adjustElementScale(document.documentElement.clientWidth);
-    dispatch(setActiveId(''));
-    dispatch(setHoveringId(''));
+    if (!isMobileDevice) {
+      adjustElementScale(document.documentElement.clientWidth);
+      dispatch(setActiveId(""));
+      dispatch(setHoveringId(""));
+    }
   };
 
   const resetOrbitSelection = () => {
-    dispatch(setActiveId(''));
-    dispatch(setHoveringId(''));
-}
+    dispatch(setActiveId(""));
+    dispatch(setHoveringId(""));
+  };
 
   function adjustElementScale(windowWidth: number) {
     const earth_orbits_container = document.getElementById(
@@ -61,9 +71,10 @@ export const App: React.FC = () => {
             ""
           );
           earth_orbits_container.style.transform =
-            updatedTransform + " " + "scale(" + {earthOrbitsScale} + ")";
+            updatedTransform + " " + "scale(" + { earthOrbitsScale } + ")";
         } else {
-          earth_orbits_container.style.transform = "scale(" + {earthOrbitsScale} + ")";
+          earth_orbits_container.style.transform =
+            "scale(" + { earthOrbitsScale } + ")";
         }
 
         // Remove any transform origin that may have been set for a fly transition, will cause the element to be off center
@@ -92,34 +103,16 @@ export const App: React.FC = () => {
         } else {
           earth_orbits_container.style.transform = scale;
         }
-      } else if (windowWidth <= 500) {
-        const loss = 500 - windowWidth;
-        const percentLoss = Math.round(loss / 5);
-        const scaleNum = 1 - percentLoss / 100;
-
-        // Set the scale in the redux store
-        dispatch(setEarthOrbitsScale(scaleNum));
-
-        const scale = "scale(" + scaleNum + ")";
-
-        // Check if the element already has been scaled
-        const existingTransform = earth_orbits_container.style.transform;
-        if (existingTransform && existingTransform.includes("scale(")) {
-          const updatedTransform = existingTransform.replace(
-            /scale\([^)]*\)\s*/g,
-            ""
-          );
-          earth_orbits_container.style.transform =
-            updatedTransform + " " + scale;
-        }
       }
     }
   }
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    //adjust for initial window width
-    adjustElementScale(document.documentElement.clientWidth);
+    if (!isMobileDevice) {
+      window.addEventListener("resize", handleResize);
+      //adjust for initial window width
+      adjustElementScale(document.documentElement.clientWidth);
+    }
   }, []);
 
   useEffect(() => {
@@ -127,7 +120,7 @@ export const App: React.FC = () => {
       const element = document.getElementById(
         flyToId + "_sat_" + currentStaticImg
       );
-      console.log('currentStaticImg: ', currentStaticImg);
+      console.log("currentStaticImg: ", currentStaticImg);
       element?.classList.remove("static_satellite_closeup");
       // Re-add the class with a slight delay to restart the animation
       setTimeout(() => {
@@ -142,41 +135,35 @@ export const App: React.FC = () => {
         {isSplashScreen ? (
           <SplashScreen />
         ) : (
-          <><div className="reset_orbit_selection_container">
-              <div
-                className="reset_selection top"
-                onClick={resetOrbitSelection}
-              ></div>
-              <div
-                className="reset_selection bottom"
-                onClick={resetOrbitSelection}
-              ></div>
-            </div>
-            <div className="nav_container">
-                {!flyToId && (
-                  <div className="orbit_title_container">
-                    {orbitIds.map((id, index) => {
-                      return (
-                        <OrbitTitleGroup
-                          key={id}
-                          id={id}
-                          tabIndex={index} />
-                      );
-                    })}
+          <div className="reset_orbit_selection_container">
+            <div
+              className="reset_selection top"
+              onClick={resetOrbitSelection}
+            ></div>
+            <div
+              className="reset_selection bottom"
+              onClick={resetOrbitSelection}
+            ></div>
+          </div>
+        )}
+        {showNavBar && (
+          <div className="nav_container">
+            {!flyToId && (
+              <div className="orbit_title_container">
+                {orbitIds.map((id, index) => {
+                  return <OrbitTitleGroup key={id} id={id} tabIndex={index} />;
+                })}
 
-                    <div className="popup_line_container">
-                      {orbitIds.map((id) => {
-                        return <PopUpLine key={id + "_popupline"} id={id} />;
-                      })}
-                    </div>
-                  </div>
-                )}
+                <div className="popup_line_container">
+                  {orbitIds.map((id) => {
+                    return <PopUpLine key={id + "_popupline"} id={id} />;
+                  })}
+                </div>
+              </div>
+            )}
 
-                {!flyToId && activeId && (
-                  <PopUpText />
-                )}
-              </div></>
-
+            {!flyToId && activeId && <PopUpText />}
+          </div>
         )}
 
         <EarthOrbitsContainer />
@@ -191,7 +178,7 @@ export const App: React.FC = () => {
             />
             <OrbitCloseupText />
             {currentStaticImg ? (
-              <StaticImgCloseup key={flyToId + "_sat_" + currentStaticImg}/>
+              <StaticImgCloseup key={flyToId + "_sat_" + currentStaticImg} />
             ) : (
               <SingleOrbitAnimationDetails
                 id={flyToId}
@@ -201,7 +188,6 @@ export const App: React.FC = () => {
             )}
           </>
         ) : null}
-
       </div>
     </Provider>
   );
