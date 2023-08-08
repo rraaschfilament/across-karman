a1680d6caaa1ed9e938e19df7c38fdbfe7f80ed3
Kristen San Martin <ksanmartin@filamentgames.com>
2023-08-08 12:29:52 -0700

    8-8 - App.css - styling satellite_image_container, adjusting height of satellite_detail and satellite_popup windows StaticImgCloseup - add ShowIf loading, add satellite_static_image_container

--- src/components/StaticImgCloseup.tsx
+++ src/components/StaticImgCloseup.tsx
@@ -1,24 +1,29 @@
-import useImage from '../hooks/useImage';
-import { useSelector } from 'react-redux';
+import useImage from "../hooks/useImage";
+import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
-
+import { ShowIf } from "./ShowIf";
 
 const StaticImgCloseup: React.FC = () => {
+  const flyToId = useSelector((state: RootState) => state.app.flyToId);
+  const currentStaticImg = useSelector(
+    (state: RootState) => state.app.currentStaticImg
+  );
 
-    const flyToId = useSelector((state: RootState) => state.app.flyToId);
-    const currentStaticImg = useSelector((state: RootState) => state.app.currentStaticImg);
+  const imageName = flyToId + "_sat_" + currentStaticImg;
+  const { image, loading } = useImage(imageName);
 
-    const imageName = flyToId + '_sat_' + currentStaticImg;
-    const {image} = useImage(imageName);
+  return (
+    <ShowIf value={!loading}>
+      <div className="static_satellite_image_container">
+        <img
+          src={image}
+          id={imageName}
+          alt={imageName}
+          className="static_satellite_closeup"
+        />
+      </div>
+    </ShowIf>
+  );
+};
 
-    
-    return (
-
-            <div >
-            <img src={image} id={imageName} alt={imageName} />
-            </div>
-
-    )
-}
-
-export default StaticImgCloseup
+export default StaticImgCloseup;
